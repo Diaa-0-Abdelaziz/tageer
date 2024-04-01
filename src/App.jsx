df39c6e9bde7baca27ec1fc999d268be355aad4e
Diaa-0-Abdelaziz <diaamohamedabelaziz@gmail.com>
2024-04-01 21:04:48 +0200

    modified:   src/App.jsx 	new file:   src/Your image mask.png 	deleted:    src/images/Screen Shot 2022-04-23 at 11.19.06 PM.png 	new file:   src/images/Your image mask@2x000.png 	new file:   src/pages/rent-car-with-driver-details/components/RentSuvDubai/RentSuvDubai.jsx 	new file:   src/pages/rent-car-with-driver-details/components/RentSuvDubai/RentSuvDubaiCursel/RentSuvDubaiCursel.css 	new file:   src/pages/rent-car-with-driver-details/components/RentSuvDubai/RentSuvDubaiCursel/RentSuvDubaiCursel.css.map 	new file:   src/pages/rent-car-with-driver-details/components/RentSuvDubai/RentSuvDubaiCursel/RentSuvDubaiCursel.jsx 	new file:   src/pages/rent-car-with-driver-details/components/RentSuvDubai/RentSuvDubaiCursel/RentSuvDubaiCursel.scss 	new file:   src/pages/rent-car-with-driver-details/rentCarWithDriverDetails.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Layout from './Layout/Layout';
 import Yachts from './pages/Yachts/Yachts';
 import YachtsDetails from './pages/YachtsDetails/YachtsDetails';
 import RentCarWithDrivers from './pages/RentCarWithDriver/RentCarWithDrivers';
+import RentCarWithDriverDetails from './pages/rent-car-with-driver-details/rentCarWithDriverDetails';
 
 function App() {
   let routes =  createBrowserRouter([
@@ -16,6 +17,7 @@ function App() {
         {path:'yachts', element:<Yachts/>},
         {path:'rentCarWithDriver', element:<RentCarWithDrivers/>},
         {path:'YachtsDetails/:id', element:<YachtsDetails/>},
+        {path:'RentCarWithDriverDetails/:id', element:<RentCarWithDriverDetails/>},
         // {path:'*', element:<Notfound/>}
       ]
     }
---
+++ src/pages/rent-car-with-driver-details/rentCarWithDriverDetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import RentSuvDubai from './components/RentSuvDubai/RentSuvDubai'
+import FAQ from '../Home/component/FAQ/FAQ'
+import BESTSERVICES from '../Home/component/BEST-SERVICES/BESTSERVICES'
+
+export default function RentCarWithDriverDetails() {
+  return (
+   <>
+   <RentSuvDubai/>
+   <BESTSERVICES/>
+   <FAQ/>
+   </>
+  )
+}
