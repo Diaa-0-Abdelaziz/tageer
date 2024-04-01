55e0ee2660ef423195c2dcdf3942e32c7451af4f
Diaa-0-Abdelaziz <diaamohamedabelaziz@gmail.com>
2024-04-01 18:50:17 +0200

    modified:   src/pages/YachtsDetails/YachtsDetails.jsx

--- src/pages/YachtsDetails/YachtsDetails.jsx
+++ src/pages/YachtsDetails/YachtsDetails.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import RentSuvDubai from './components/RentSuvDubai/RentSuvDubai'
+import FAQ from '../Home/component/FAQ/FAQ'
 
 export default function YachtsDetails() {
   return (
    <>
    <RentSuvDubai/>
+   <FAQ/>
    </>
   )
 }
