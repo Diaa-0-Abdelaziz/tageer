013efce2bcd85be66d9d84f82ce672096f37f839
Diaa-0-Abdelaziz <diaamohamedabelaziz@gmail.com>
2024-04-02 05:34:59 +0200

    modified:   src/pages/Blog/components/BEST-SERVICES/BESTSERVICES.jsx

--- src/pages/Blog/components/BEST-SERVICES/BESTSERVICES.jsx
+++ src/pages/Blog/components/BEST-SERVICES/BESTSERVICES.jsx
@@ -7,10 +7,10 @@ export default function BESTSERVICES() {
     <section className='BESTSERVICES'>
 <div className="container overflow-hidden">
     <div className="row d-flex justify-content-center overflow-hidden">
-        <div className="col-8">
+        <div className="col-md-8 col-sm-12">
         <img src={img_your_image_mask} alt={img_your_image_mask} className=' w-100' />
         </div>
-        <div className="col-8">
+        <div className="col-md-8 col-sm-12">
             <h3>We will allways provide the best services</h3>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eius necessitatibus aspernatur dolorum aliquid facilis exercitationem deserunt nemo eveniet itaque, magnam nisi tenetur molestiae dolor vitae corporis! Esse voluptas necessitatibus autem labore quod, earum tenetur corrupti reprehenderit aut suscipit natus. Commodi natus cum at esse molestiae iste dicta velit enim?
