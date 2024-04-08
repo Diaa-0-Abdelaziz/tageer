42e2fd6d883759843907a5de2f71d5a6496982de
Diaa-0-Abdelaziz <diaamohamedabelaziz@gmail.com>
2024-04-08 20:50:47 +0200

    modified:   src/App.css 	modified:   src/App.css.map 	modified:   src/App.scss 	modified:   src/Layout/Layout.jsx 	new file:   src/ScrollToTopButton/ScrollToTopButton.jsx 	renamed:    src/images/kisspng-bugatti-chiron-bugatti-veyron-koenigsegg-agera-r-c-bugatti-top-view-5a71e7ef975a24.54748771151741438362 (2).png -> src/images/car.png 	modified:   src/pages/Home/component/RentSUVLuxury/RentSUVLuxury.jsx

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -3,12 +3,14 @@ import Navbar from '../Navbar/Navbar'
 import { Outlet } from 'react-router-dom'
 import Footer from '../Footer/Footer'
 import Breadcrumb from '../Breadcrumb/Breadcrumb'
+import ScrollToTopButton from '../ScrollToTopButton/ScrollToTopButton'
 
 export default function Layout() {
   return (
     <>
     <Navbar/>
     <Breadcrumb/>
+    <ScrollToTopButton/>
     <Outlet/>
     <Footer/>
     </>
---
+++ src/ScrollToTopButton/ScrollToTopButton.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import car from '../images/car.png'
+function ScrollToTopButton() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Function to handle scroll event
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY;
+    // Show the button if the user scrolls down 200 pixels from the top
+    if (scrollPosition > 200) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  // Function to handle scroll to top
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // Smooth scrolling behavior
+    });
+  };
+
+  // Add event listener when the component mounts
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    // Clean up the event listener when the component unmounts
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <div className={`scroll-to-top-button ${isVisible ? 'show' : ''}`} onClick={scrollToTop}>
+     <img src={car} alt={car} width={50} height={100} />
+    </div>
+  );
+}
+
+export default ScrollToTopButton;
