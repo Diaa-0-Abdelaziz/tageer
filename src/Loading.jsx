908b9cbe93d412d2fe64f070d9b0e04c6dda239c
Diaa-0-Abdelaziz <diaamohamedabelaziz@gmail.com>
2024-04-09 10:57:13 +0200

    modified:   src/Loading.jsx

--- src/Loading.jsx
+++ src/Loading.jsx
@@ -5,7 +5,7 @@ export default function Loading() {
     
     <>
     <section className=' position-absolute top-0 bottom-0 start-0 end-0 d-flex align-items-center justify-content-center' style={{backgroundColor:"#3D1F50", zIndex:"9999999"}}>
-       <img src={logo} width={200} height={200} alt={logo} />
+       <img src={logo} width={200} height={200} alt={logo} loading='lazy' />
     </section>
     </>
   )
