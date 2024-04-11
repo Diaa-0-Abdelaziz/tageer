36d7cc058d3d75f323c31ddd069bfde45f7caa4e
Diaa-0-Abdelaziz <diaamohamedabelaziz@gmail.com>
2024-04-11 10:52:03 +0200

    modified:   src/pages/Home/component/Search/Search.jsx

--- src/pages/Home/component/Search/Search.jsx
+++ src/pages/Home/component/Search/Search.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { BsSearch } from "react-icons/bs";
-import LazyLoad from 'react-lazyload';
 import { GrFormPrevious } from "react-icons/gr";
 
 import imgCover from "../../../../images/car-subscription-new-image@2x.png"
@@ -10,9 +9,9 @@ export default function Search() {
     <>
     <section className='main_content  position-relative'>
     <div className='search position-relative'>
-      <LazyLoad>
+
         <img src={imgCover} alt={imgCover} loading='lazy'/>
-      </LazyLoad>
+  
         <div className='position-absolute top-0 bottom-0 start-0 end-0 cover_color'></div>
     </div>
        <div className='container form position-absolute top-50 start-50 translate-middle d-flex justify-content-center'>
