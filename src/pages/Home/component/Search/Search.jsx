21598afa546dc4be668455207fe2679c9dd244ce
Diaa-0-Abdelaziz <diaamohamedabelaziz@gmail.com>
2024-04-15 06:56:08 +0200

    modified:   src/pages/Home/component/Search/Search.jsx

--- src/pages/Home/component/Search/Search.jsx
+++ src/pages/Home/component/Search/Search.jsx
@@ -11,7 +11,7 @@ export default function Search() {
     <section className='main_content  position-relative'>
     <div className='search position-relative'>
 
-        <img src={imgCover} alt={imgCover} loading='lazy'/>
+        <img src={imgCover} alt={imgCover}/>
 
         <div className='position-absolute top-0 bottom-0 start-0 end-0 cover_color'></div>
     </div>
